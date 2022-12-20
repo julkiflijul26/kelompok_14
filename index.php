8de285308e915a74e5098d6607f7318fde9721c0
julkiflijul26 <116624000+julkiflijul26@users.noreply.github.com>
2022-12-20 22:10:58 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
+<?php
+session_start();
+
+if( !isset($_SESSION["login"]) ) {
+	header("Location: login.php");
+	exit;
+}
+
+?>
 <?php 
 	include('dbconnect.php');
+	include('functions.php');
 	include ('harga.php');
 
 	$query = "SELECT * FROM buku";
@@ -9,127 +19,103 @@
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-	<title>INI PERPUS</title>
+	<title>ini perpus</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
-		integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-	<style>
-		html {
-			height: auto;
-		}
-		body {
-			margin: 0;
-			padding: 0;
-			font-family: sans-serif;
-			background: linear-gradient(#141e30, #243b55);
-		}
-
-		h1 {
-			padding: 10px 20px;
-			background-color: #111b28;
-			color: #24B4C1;
-			font-size: 20px;
-			text-decoration: none;
-			text-transform: uppercase;
-			margin-top: -25px;
-			letter-spacing: 1px;
-			border-radius: 10px;
-		}
-	</style>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 </head>
-
 <body>
-	<nav class="navbar navbar-light" style="background-color: #03e9f4;">
-		<a class="navbar-brand" href="#table">
-			<img src="logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
-			Perpustakaan
-		</a>
-	</nav>
+<nav class="navbar navbar-light bg-primary">
+  <a class="navbar-brand" href="#table">
+    <img src="logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+    ini perpus
+  </a>
+</nav>
 	<center>
-		<div class="container">
-			<div id="tambah" class="card-header" style="width: 50rem;">
-				<div class="card-body text-center">
-					<h1 class="card-title">Tambah buku</h1>
-					<form role="form" action="tambah.php" method="post">
-						<div class="form-group">
-							<label style="color: #fff;">Judul</label>
-							<input type="text" name="judul_bk" class="form-control">
-						</div>
-						<div class="form-group">
-							<label style="color: #fff;">Penerbit</label>
-							<input type="text" name="terbit_bk" class="form-control">
-						</div>
-						<div class="form-group">
-							<label style="color: #fff;">Genre</label>
-							<input type="text" name="genre_bk" class="form-control">
-						</div>
-						<div class="form-group">
-							<label style="color: #fff;">Harga</label>
-							<input type="text" name="harga_bk" class="form-control">
-						</div>
-						<div class="form-group">
-							<label style="color: #fff;" for="inputState">Stok</label>
-							<select id="inputState" name="stok_bk" class="form-control" required>
-								<option value="" selected>Pilih...</option>
-								<option value="tersedia">Tersedia</option>
-								<option value="kosong">Kosong</option>
-							</select>
-						</div>
-						<button type="submit" class="btn btn-info btn-block">Tambah Buku</button>
-					</form>
+	<div class="container">
+	<div id="tambah" class="card-header" style="width: 50rem;">
+  <div class="card-body text-center">
+    <h1 class="card-title">tambah buku</h1>
+				<form role="form" action="tambah.php" method="post">
+					<div class="form-group">
+						<label>Judul</label>
+						<input type="text" name="judul_bk" class="form-control">
+					</div>
+					<div class="form-group">
+						<label>Penerbit</label>
+						<input type="text" name="terbit_bk" class="form-control">
+					</div>
+					<div class="form-group">
+						<label>genre</label>
+						<input type="text" name="genre_bk" class="form-control">
+					</div>
+					<div class="form-group">
+						<label>Harga</label>
+						<input type="text" name="harga_bk" class="form-control">
+					</div>
+					<div class="form-group">
+					<label for="inputState">stok</label>
+					<select id="inputState" name="stok_bk" class="form-control" required>
+						<option value="" selected>Pilih...</option>
+						<option value="tersedia">tersedia</option>
+						<option value="kosong">kosong</option>
+					</select>
 				</div>
-			</div>
-		</div>
+					<button type="submit" class="btn btn-info btn-block">Tambah Buku</button>					
+				</form>
+  </div>
+</div>
+	</div>
 	</center>
 	<br>
-	<div id="table" class="col-sm-12" style="color: #fff;">
-		<h3>Daftar Buku</h3>
-		<table class="table table-striped table-hover">
-			<thead>
-				<tr style="color: #fff;">
-					<th>No</th>
-					<th>Judul Buku</th>
-					<th>Penerbit Buku</th>
-					<th>Jenis Buku</th>
-					<th>Harga Buku</th>
-					<th>Stok Buku</th>
-					<th>Aksi</th>
-				</tr>
-			</thead>
-			<tbody>
-
-				<?php
+			<div id="table" class="col-sm-12">
+				<h3>Daftar Buku</h3>
+				<table class="table table-striped table-hover">
+					<thead>
+						<tr>
+							<th>No</th>
+							<th>Judul Buku</th>
+							<th>Penerbit Buku</th>
+							<th>Jenis Buku</th>
+							<th>Harga Buku</th>
+							<th>Stok Buku</th>
+							<th>Aksi</th>
+						</tr>
+					</thead>
+					<tbody> 
+						
+						<?php
 							$no = 1;  
 							while ($row = mysqli_fetch_assoc($result)) {
 						?>
-				<tr style="color: #fff;">
-					<td><?php echo $no++; ?></td>
-					<td><?php echo $row['judul_buku']; ?></td>
-					<td><?php echo $row['penerbit_buku']; ?></td>
-					<td><?php echo $row['genre_buku']; ?></td>
-					<td><?php echo rupiah ($row['harga_buku']); ?></td>
-					<td><?php echo $row['stok_buku']; ?></td>
-					<td>
-						<a href="editform.php?id=<?php echo $row['id_buku'];?>" class="btn btn-success"
-							role="button">Edit</a>
-						<a href="delete.php?id=<?php echo $row['id_buku']?>" class="btn btn-danger"
-							role="button">Delete</a>
-					</td>
-				</tr>
+						<tr>
+							<td><?php echo $no++; ?></td>
+							<td><?php echo $row['judul_buku']; ?></td>
+							<td><?php echo $row['penerbit_buku']; ?></td>
+							<td><?php echo $row['genre_buku']; ?></td>
+							<td><?php echo rupiah ($row['harga_buku']); ?></td>
+							<td><?php echo $row['stok_buku']; ?></td>
+							<td>
+								<a href="editform.php?id=<?php echo $row['id_buku'];?>" class="btn btn-success" role="button">Edit</a>
+								<a href="delete.php?id=<?php echo $row['id_buku']?>" class="btn btn-danger" role="button">Delete</a>
+							</td>
+						</tr>
 
-				<?php
+						<?php
 							}
 						?>
-			</tbody>
-		</table>
-	</div>
+					</tbody>
+				</table>
+			</div>
+			<div class="card-body text-center">
+				<form role="form" action="logout.php" method="post">
+					<button type="submit" class="btn btn-danger btn-block">logout</button>					
+				</form>
+  </div>
 
-	</div>
-
+		</div>
+		
 	</div>
 </body>
-
-</html>
+</html> 
